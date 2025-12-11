f07f4e5847668c95e2de82902439a38e65ad5dde
Juan <jmbretana@gmail.com>
2025-12-11 16:13:49 -0300

    -

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,5 @@ declare module "*.svg" {
   const content: string;
   export default content;
 }
+
+declare const PUBLIC_API_URL: string;
--- src/utils/environment.tsx
+++ src/utils/environment.tsx
@@ -1,4 +1,4 @@
-const API_URL = process.env.PUBLIC_API_URL || "http://localhost:3001/api";
+const API_URL = PUBLIC_API_URL || "http://localhost:3001/api";
 
 export const getApiEnvironment = () => {
   console.log("API URL:", API_URL);
