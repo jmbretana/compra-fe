0f179ff1d25db3752e45b1b3153b4d9d273f28f9
Juan <jmbretana@gmail.com>
2025-12-11 16:09:42 -0300

    -

--- src/utils/environment.tsx
+++ src/utils/environment.tsx
@@ -1,13 +1,6 @@
-export const API_SERVER_LOCAL = "http://localhost:3001/api"; // Acción para iniciar una solicitud
-export const API_SERVER_PROD =
-  "https://appsalve-api.netlify.app/.netlify/functions/api"; // Acción para el éxito al obtener todas las solicitudes
+const API_URL = process.env.PUBLIC_API_URL || "http://localhost:3001/api";
 
 export const getApiEnvironment = () => {
-  let apiURLServer = "http://localhost:3001/api";
-
-  if (window.location.hostname !== "localhost") {
-    apiURLServer = "https://appsalve-api.netlify.app/.netlify/functions/api";
-  }
-
-  return apiURLServer;
+  console.log("API URL:", API_URL);
+  return API_URL;
 };
